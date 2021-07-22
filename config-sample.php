0d51d5e4cce46d64a01067d80ca0efc35c1b144a
nickbethke <nickbethke5599@gmail.com>
2021-07-22 20:48:50 +0200

    config-sample

---
+++ config-sample.php
@@ -0,0 +1,17 @@
+<?php
+
+define("PRODUCT_URL_PREFIX", "p");
+define("CATEGORY_URL_PREFIX", "c");
+
+define("DB_USER", "DB_USER");
+define("DB_PASSWORD", "DB_PASSWORD");
+define("DB_NAME", "DB_NAME");
+define("DB_HOST", "DB_HOST");
+
+
+
+
+
+
+
+require_once "settings.php";
